94d27734ac27c16d3627d4c51242a4a4e63f1e9f
sha583 <sharansusalavani@gmail.com>
2024-12-17 19:22:05 -0800

    Create nested if else.py

---
+++ nested if else.py
@@ -0,0 +1,16 @@
+while True:
+    print('1.payment failed')
+    print('2.profile and privacy')
+    print('3.money transfer')
+    print('4.exit')
+    user_inp=int(input('enter your choice'))
+    if user_inp==1:
+        print('welcome to failed payment failed')
+    elif user_inp==2:
+        print('welcome to profile and privacy')
+    elif user_inp==3:
+        print('welcome to money transfer')  
+    elif user_inp==4:  
+        print('exit')
+        break    
+    
