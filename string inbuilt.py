1946675a755c2cb2493e1065f3e93ca1385b5005
sha583 <sharansusalavani@gmail.com>
2024-12-23 18:45:24 -0800

    Create string inbuilt.py

---
+++ string inbuilt.py
@@ -0,0 +1,20 @@
+print('hello😍'.upper())
+
+print('A','ord--->',ord('A'))
+
+val='ABC'
+for x in val:
+    ord_val=ord(x)
+    cov_chr=ord_val+32
+    print(chr(conv_chr))
+
+
+val='abc'
+for x in val:
+    ord_val=ord(x)
+    cov_chr=ord_val-32
+    print(chr(conv_chr))
+
+
+
+
