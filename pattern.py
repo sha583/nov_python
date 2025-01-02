cd25f0d8ed827d3a4f6c09bfd5d9d67005642b1d
sha583 <sharansusalavani@gmail.com>
2025-01-02 07:48:56 -0800

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,31 @@
+# for x in range(3):
+#     for x in range(2):
+#         print("*",end='')
+
+
+# for row in range(4):
+#     for col in range(row+1):
+#         print("*",end=' ')
+#     print()
+
+# for row in range(5):
+#     for col in range(row+1):
+#         print('*',end='')
+#     print()
+
+# for row in range(5):
+#     for col in range(row+1):
+#         print(row,end=' ')
+#     print()
+
+val=[1,87,79]
+for x in range(len(val)):
+    for y in range(len(val)):
+        if val[x]<val[y]:
+            val[x],val[y]=val[y],val[x]
+        else:
+            val[x],val[y]=val[x],val[y]  
+print(val)       
+
+  
+
