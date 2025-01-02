02e46bbcffc08b81cf4f1c786a46f344e73d0d6b
sha583 <sharansusalavani@gmail.com>
2025-01-02 07:48:25 -0800

    Create lamda.py

---
+++ lamda.py
@@ -0,0 +1,27 @@
+#lamda function
+
+# # mul=lambda x,y:x*y
+# print(mul(5,2))
+
+
+#zip function
+
+# list1=[1,2,3]
+# list2=['a','b','c']
+# zipped=zip(list1,list2)
+# print(tuple(zipped))
+
+
+# join function
+
+str1='kiran'
+str2='rames'
+str3=str1.join(str2)
+print(str3)
+
+
+
+str = "Rohan"
+str2 = "ab"
+str2 = str.join(str2)
+print(str2)
