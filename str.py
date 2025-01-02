2202295185a8c8a6d2ecae9228c3b026f5453e8c
sha583 <sharansusalavani@gmail.com>
2025-01-02 07:49:04 -0800

    Update str.py

--- str.py
+++ str.py
@@ -1,43 +1,81 @@
-cor_ans=0
-wrong_ans=0
-print('1.what is the capital of india')
-user_ans=input('enter ans:').lower()
-print(user_ans)
-if user_ans=='delhi':
-    cor_ans+=1
-    print('correct ans')
-else:
-    wrong_ans+=1
-    print('wrong answer')
-print('percentage',((cor_ans/(cor_ans+wrong_ans))*100))  
-print('1.what is the capital of karnataka')
-user_ans=input('enter ans:').lower()
-print(user_ans)
-if user_ans=='bangalore':
-    cor_ans+=1
-    print('correct ans')
-else:
-    wrong_ans+=1
-    print('wrong answer')
-print('percentage',((cor_ans/(cor_ans+wrong_ans))*100))       
-print('1.what is the national animal')
-user_ans=input('enter ans:').lower()
-print(user_ans)
-if user_ans=='tiger':
-    cor_ans+=1
-    print('correct ans')
-else:
-    wrong_ans+=1
-    print('wrong answer')
-print('percentage',((cor_ans/(cor_ans+wrong_ans))*100)) 
-print('1.what is the national bird')
-user_ans=input('enter ans:').lower()
-print(user_ans)
-if user_ans=='peacock':
-    cor_ans+=1
-    print('correct ans')
-else:
-    wrong_ans+=1
-    print('wrong answer')
-print('percentage',((cor_ans/(cor_ans+wrong_ans))*100))
+# cor_ans=0
+# wrong_ans=0
+# print('1.what is the capital of india')
+# user_ans=input('enter ans:').lower()
+# print(user_ans)
+# if user_ans=='delhi':
+#     cor_ans+=1
+#     print('correct ans')
+# else:
+#     wrong_ans+=1
+#     print('wrong answer')
+# print('percentage',((cor_ans/(cor_ans+wrong_ans))*100))  
+# print('1.what is the capital of karnataka')
+# user_ans=input('enter ans:').lower()
+# print(user_ans)
+# if user_ans=='bangalore':
+#     cor_ans+=1
+#     print('correct ans')
+# else:
+#     wrong_ans+=1
+#     print('wrong answer')
+# print('percentage',((cor_ans/(cor_ans+wrong_ans))*100))       
+# print('1.what is the national animal')
+# user_ans=input('enter ans:').lower()
+# print(user_ans)
+# if user_ans=='tiger':
+#     cor_ans+=1
+#     print('correct ans')
+# else:
+#     wrong_ans+=1
+#     print('wrong answer')
+# print('percentage',((cor_ans/(cor_ans+wrong_ans))*100)) 
+# print('1.what is the national bird')
+# user_ans=input('enter ans:').lower()
+# print(user_ans)
+# if user_ans=='peacock':
+#     cor_ans+=1
+#     print('correct ans')
+# else:
+#     wrong_ans+=1
+#     print('wrong answer')
+# print('percentage',((cor_ans/(cor_ans+wrong_ans))*100))
 
+#quiz application 
+
+# q=['1.what is the capital of india',
+# '2.what is the national animal',
+# '3.what is the capital of karnataka',
+# '4.what is the national bird',
+# '5.what is the national sport']
+# ans=['delhi','tiger','bangalore','peacock','hockey']
+# for x in q:
+#     print(x)
+#     user_ans=input('enter the answer:').lower
+#     if user_ans in ans:
+#         print('correct ans')
+#     else:
+#         print('wrong ans')
+
+#3.
+# corr_ans=0
+# wrong_ans=0
+# q=['1.what is the capital of india',
+# '2.what is the national animal',
+# '3.what is the capital of karnataka',
+# '4.what is the national bird',
+# '5.what is the national sport']
+# ans=['delhi','tiger','bangalore','peacock','hockey']
+# for x in range(len(q)):
+#     print(q[x])
+#     user_ans=input('enter the answer:').lower
+#     if user_ans==ans[x]:
+#         corr_ans+=1
+#         print('correct ans')
+#     else:
+#         wrong_ans+=1
+#         print('wrong ans')
+# print("percentage",((corr_ans/(corr_ans+wrong_ans))*100 ))    
+  
+att=[1,44,66,88,[66,77],90]
+print(att[-1])
