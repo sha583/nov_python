0eee782741dbb81c7bb3880ae4a45d620aa17676
sha583 <sharansusalavani@gmail.com>
2025-01-29 08:40:03 -0800

    Create string indexing and slicing.py

---
+++ string indexing and slicing.py
@@ -0,0 +1,12 @@
+# val='rama'
+# print(val[2])
+
+# sales='rameshwar'
+# print(sales[6:7])
+# print(sales[-3:-1])
+
+# val='nitin'
+# if val[::1]==val[::-1]:
+#     print('palindrome')
+# else:
+#     print('not a palindrome')    
