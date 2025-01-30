49828bf5501dc4dd98a3ad11caa7eef94f571dde
sha583 <sharansusalavani@gmail.com>
2025-01-30 06:35:58 -0800

    Create password code.py

---
+++ password code.py
@@ -0,0 +1,24 @@
+username=input('enter the username: ')
+password=int(input('enter the password:'))
+
+user=['rohit']
+passw=[1234]
+user_name=[]
+passwo=[]
+
+def create_acc():
+   user1=input("Enter the name")
+   passwor=int(input("Enter the password"))
+   user1.append(user_name)
+   passwor.append(passwo)
+   print("Account created successfully")
+if user==username and passw==password:
+      print ('sucessfull')
+else:
+      print('error')
+      create_acc()
+
+      
+
+
+    
